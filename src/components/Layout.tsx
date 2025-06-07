c7ca9140f87597377e4b896d294c13c4764d7f42
ben-jamin-chan <chanbenjamin.tl@gmail.com>
2025-06-07 16:34:07 +0800

    Updated logout button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -58,7 +58,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
                 <p className="text-sm font-medium text-gray-900">{user?.name}</p>
                 <button
                   onClick={logout}
-                  className="text-xs font-medium text-gray-500 hover:text-gray-700 transition-colors"
+                  className="text-xs font-medium text-gray-500 hover:text-gray-700 transition-colors border border-gray-500 px-3 py-1 rounded-md"
                 >
                   Logout
                 </button>
